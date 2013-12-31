5060be67efe56b7febe25482f5b194bf62438dab
Vinod Kisanagaram <kvr2277@gmail.com>
2013-12-31 00:48:31 -0500

    Dynamic retrieval of data.

--- NICUtil/src/main/java/com/dbhelp/util/OracleHelper.java
+++ NICUtil/src/main/java/com/dbhelp/util/OracleHelper.java
@@ -77,39 +77,4 @@ public class OracleHelper {
 		OracleHelper oh = new OracleHelper();
 		oh.getAddress("001");
 	}
-	
-	
-	
-	/*public static void main2(String[] argv) {
-		 
-		System.out.println("-------- Oracle JDBC Connection Testing ------"); 
-		try {
- 
-			Class.forName("oracle.jdbc.driver.OracleDriver");
- 
-		} catch (ClassNotFoundException e) {
-			System.out.println("Where is your Oracle JDBC Driver?");
-			e.printStackTrace();
-			return;
-		}
- 
-		System.out.println("Oracle JDBC Driver Registered!");
-		Connection connection = null;
- 
-		try {
-			connection = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:orcl", "vinoracle",
-					"password");
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
-		}
- 
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
-		}
-	}*/
 }
